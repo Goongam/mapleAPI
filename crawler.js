145f1ffa7d80c99f47bd702b3874bc189c1751ee
Goongam <dlwjddn341@naver.com>
2022-07-31 06:12:25 +0900

    구조 수정

--- crawler.js
+++ crawler.js
@@ -2,21 +2,21 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-async function main() {
-  // ❶ HTML 로드하기
-  const resp = await axios.get(
-    'https://yjiq150.github.io/coronaboard-crawling-sample/dom'
-  );
+// async function main() {
+//   // ❶ HTML 로드하기
+//   const resp = await axios.get(
+//     'https://yjiq150.github.io/coronaboard-crawling-sample/dom'
+//   );
 
-  const $ = cheerio.load(resp.data); // ❷ HTML을 파싱하고 DOM 생성하기
-  const elements = $('.slide p');    // ❸ CSS 셀렉터로 원하는 요소 찾기
-  // ➍ 찾은 요소를 순회하면서 요소가 가진 텍스트를 출력하기
-  console.log(elements);
-  elements.each((idx, el) => {
-    // ❺ text() 메서드를 사용하기 위해 Node 객체인 el을 $로 감싸서 cheerio 객체로 변환
-    console.log($(el).text());
-  });
-}
+//   const $ = cheerio.load(resp.data); // ❷ HTML을 파싱하고 DOM 생성하기
+//   const elements = $('.slide p');    // ❸ CSS 셀렉터로 원하는 요소 찾기
+//   // ➍ 찾은 요소를 순회하면서 요소가 가진 텍스트를 출력하기
+//   console.log(elements);
+//   elements.each((idx, el) => {
+//     // ❺ text() 메서드를 사용하기 위해 Node 객체인 el을 $로 감싸서 cheerio 객체로 변환
+//     console.log($(el).text());
+//   });
+// }
 
 // main();
 const client = axios.create({
@@ -32,47 +32,38 @@ async function getSearchURL(nick){
     const cafe_resp = await client.get(url);
     const $ = cheerio.load(cafe_resp.data);
     const editerPick = $(".search_com_chk");
-    return "https://maplestory.nexon.com"+editerPick.find("td.left > dl > dt > a").attr("href");
+    if($(editerPick).text() === "") return "NULL"
+    else return getinfoURL("https://maplestory.nexon.com"+editerPick.find("td.left > dl > dt > a").attr("href"));
 }
 
-async function getinfoURL(nick){
-    const ChracterURL = await getSearchURL(nick);
+async function getinfoURL(ChracterURL){
     const resp = await client.get(ChracterURL);
     const $ = cheerio.load(resp.data);
     const data = $("#container > div.con_wrap > div.lnb_wrap > ul > li:nth-child(1) > a");
-    return "https://maplestory.nexon.com"+data.attr("href");
+    return getRankingInfo( "https://maplestory.nexon.com"+data.attr("href"));
 }
 
-async function getRankingInfo(nick){
-    const infoURL = await getinfoURL(nick);
+async function getRankingInfo(infoURL){
     const respInfo = await client.get(infoURL);
     const $ = cheerio.load(respInfo.data);
     const TotalRankRowArray = $("#container > div.con_wrap > div.contents_wrap > div > table > tbody > tr");
     
     let totalranks = TotalRankRowArray.map((index,el)=>$(el).find(":nth-child(2)").text()).toArray();
-
     return totalranks;
 
 }
 
 async function get1(nick){
     
-   const totalrankArray = await getRankingInfo(nick);
-   
-   console.log(totalrankArray);
+  //  const totalrankArray = await getRankingInfo(nick);
+   const totalrankArray = await getSearchURL(nick);
+
+  console.log(totalrankArray);
 
 }
 
-async function get2(){
-    const url = "https://goongam.github.io/note/";
-    const resp = await axios.get(url);
-    const $ = cheerio.load(resp.data);
-    const elements = $("#preset");
-    console.log(elements.html());
-    
-}
 
 
 get1("당체용");
-//메이플 랭킹페이지 맨끝:6708453
+//메이플 랭킹페이지 맨끝:6708453 //
 // get2();
